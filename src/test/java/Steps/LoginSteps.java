6faafd2a91dfcbf0197478a635af372bec7ffd23
Liudmyla Zhlobinska <l.zhlobinska@gmail.com>
2023-03-07 22:15:03 +0100

    create new project on Cucumber

---
+++ src/test/java/Steps/LoginSteps.java
@@ -0,0 +1,13 @@
+package Steps;
+
+import cucumber.api.java.en.Given;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class LoginSteps {
+    @Given("^I open login page$")
+    public void iOpenLoginPage() {
+        open("https://www.saucedemo.com/");
+    }
+}
